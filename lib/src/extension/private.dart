93e98c0b60f294d7d4fe61898a640757050e76b7
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-09-17 00:46:37 +0900

    - Use default `fold` method

--- lib/src/extension/private.dart
+++ lib/src/extension/private.dart
@@ -1,14 +1,5 @@
 part of '../../super_measurement.dart';
 
 extension _IterableOfTExtendsUnit<T extends Unit<T>> on Iterable<T> {
-  T _combine(T initialValue, T Function(T accumulator, T element) combine) {
-    T result = initialValue;
-    for (final element in this) {
-      result = combine(result, element);
-    }
-    return result;
-  }
-
-  T _combineTo(T initialValue) =>
-      _combine(initialValue..value ??= 0, (a, e) => a + e);
+  T _combineTo(T initialValue) => fold(initialValue, (a, e) => a + e);
 }
