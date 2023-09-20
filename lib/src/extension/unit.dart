de8197cb890a9c54b57f94efbc37009a9301fde5
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-09-20 12:26:30 +0900

    - Use `index` properties of `Precision` enum

--- lib/src/extension/unit.dart
+++ lib/src/extension/unit.dart
@@ -2,5 +2,5 @@ part of '../../super_measurement.dart';
 
 extension UnitExtension<T extends Unit<T>> on T {
   T withPrecision([Precision precision = Precision.two]) =>
-      this..value = value!.toDouble().toPrecision(precision.value);
+      this..value = value!.toDouble().toPrecision(precision.index + 1);
 }
--- lib/src/types/math.dart
+++ lib/src/types/math.dart
@@ -2,18 +2,15 @@ part of '../../super_measurement.dart';
 
 /// Decimal [Precision]
 enum Precision {
-  one._(1),
-  two._(2),
-  three._(3),
-  four._(4),
-  five._(5),
-  six._(6),
-  seven._(7),
-  eight._(8),
-  nine._(9);
-
-  const Precision._(this.value);
-  final int value;
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine;
 }
 
 typedef BaseType = Type;
