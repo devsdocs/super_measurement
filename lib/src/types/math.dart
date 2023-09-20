b1c9c269c6a6172a93c634dcdd86bc1233539697
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-09-20 16:44:56 +0900

    Revert "- Use `index` properties of `Precision` enum"
    This reverts commit de8197cb890a9c54b57f94efbc37009a9301fde5.

--- lib/src/types/math.dart
+++ lib/src/types/math.dart
@@ -2,15 +2,18 @@ part of '../../super_measurement.dart';
 
 /// Decimal [Precision]
 enum Precision {
-  one,
-  two,
-  three,
-  four,
-  five,
-  six,
-  seven,
-  eight,
-  nine;
+  one._(1),
+  two._(2),
+  three._(3),
+  four._(4),
+  five._(5),
+  six._(6),
+  seven._(7),
+  eight._(8),
+  nine._(9);
+
+  const Precision._(this.value);
+  final int value;
 }
 
 typedef BaseType = Type;
