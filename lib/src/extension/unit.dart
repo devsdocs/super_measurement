6904a6971df4a94bbf8c3cfc1b00a9d1cdd6dc13
devsdocs <id45.go@gmail.com>
2023-12-10 12:25:51 +0900

    If value is 0, don't bother to calculate precision

--- lib/src/extension/unit.dart
+++ lib/src/extension/unit.dart
@@ -1,6 +1,7 @@
 part of '../../super_measurement.dart';
 
 extension UnitExtension<T extends Unit<T>> on T {
-  T withPrecision([Precision precision = Precision.two]) =>
-      withValue(value!.toDouble().toPrecision(precision.value));
+  T withPrecision([Precision precision = Precision.two]) => withValue(
+        value == 0 ? 0 : value!.toDouble().toPrecision(precision.value),
+      );
 }
