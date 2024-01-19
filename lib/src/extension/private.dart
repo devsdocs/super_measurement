dda6d66931b544bcae4b5b9fd31e0b863d97bd2c
devsdocs <id45.go@gmail.com>
2024-01-20 07:45:06 +0900

    Add `convertAllTo()`

--- lib/src/extension/private.dart
+++ lib/src/extension/private.dart
@@ -7,6 +7,7 @@ extension IterableOfTExtendsUnit<T extends Unit<T>> on Iterable<T> {
   List<T> get lowestToHighest => _sort;
   List<T> get highestToLowest => _sort.reversed.toList();
   List<T> get _sort => toList()..sort();
+  List<T> convertAllTo(T unit) => map((e) => e.convertTo(unit)).toList();
   num totalValueIn(T unit) => combineTo(unit).value!;
   num averageValueIn(T unit) => totalValueIn(unit) / length;
 }
