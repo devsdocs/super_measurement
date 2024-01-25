fda3205a293a9aa7bfdc708112fa3b6bf6b0fbb3
devsdocs <id45.go@gmail.com>
2024-01-26 05:14:23 +0900

    Add some docs to extension

--- lib/src/extension/iterable_generic.dart
+++ lib/src/extension/iterable_generic.dart
@@ -1,7 +1,8 @@
 part of '../../super_measurement.dart';
 
 extension IterableOfTExtendsUnit<T extends Unit<T>> on Iterable<T> {
-  /// Combine all element in this to [unit]
+  /// Combine all element in this to [unit],
+  /// value of [unit] is ignored
   T combineTo(T unit) => fold(unit, (a, e) => a + e);
 
   /// Unit with the relatively lowest value
@@ -17,12 +18,15 @@ extension IterableOfTExtendsUnit<T extends Unit<T>> on Iterable<T> {
   List<T> get highestToLowest => _sort.reversed.toList();
   List<T> get _sort => toList()..sort();
 
-  /// Convert all element in this to [unit]
+  /// Convert all element in this to [unit],
+  /// value of [unit] is ignored
   List<T> convertAllTo(T unit) => map((e) => e.convertTo(unit)).toList();
 
-  /// Combine all element in this to [unit] and returning the value
+  /// Combine all element in this to [unit] and returning the value,
+  /// value of [unit] is ignored
   num totalValueIn(T unit) => combineTo(unit).value;
 
-  /// Combine all element in this to [unit] and returning the average value
+  /// Combine all element in this to [unit] and returning the average value,
+  /// value of [unit] is ignored
   num averageValueIn(T unit) => totalValueIn(unit) / length;
 }
